99ff2eb8f154ca0c7a04588ad97d8d12c3d1d7fd
Joeisjoejoe <joeisjoe1616@gmail.com>
2025-02-05 19:04:15 +0800

    Update Background.vue

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -2,7 +2,7 @@
   <div :class="store.backgroundShow ? 'cover show' : 'cover'">
     <img
       v-show="store.imgLoadStatus"
-      :src="/images/cyber.jpg"
+      :src="bgUrl"
       class="bg"
       alt="cover"
       @load="imgLoadComplete"
@@ -31,7 +31,7 @@ const store = mainStore();
 const bgUrl = ref(null);
 const imgTimeout = ref(null);
 const emit = defineEmits(["loadComplete"]);
-
+bgUrl.value = `/images/cyber.jpg`
 // 壁纸随机数
 // 请依据文件夹内的图片个数修改 Math.random() 后面的第一个数字
 const bgRandom = Math.floor(Math.random() * 1 + 1);
@@ -39,14 +39,16 @@ const bgRandom = Math.floor(Math.random() * 1 + 1);
 // 更换壁纸链接
 const changeBg = (type) => {
   if (type == 0) {
-    bgUrl.value = `/images/background${bgRandom}.jpg`;
+   // bgUrl.value = `/images/background${bgRandom}.jpg`;
+    bgUrl.value = `/images/cyber.jpg`;
   } else if (type == 1) {
     bgUrl.value = "https://api.dujin.org/bing/1920.php";
   } else if (type == 2) {
-    bgUrl.value = "https://api.vvhan.com/api/wallpaper/views";
+   // bgUrl.value = "https://api.vvhan.com/api/wallpaper/views";
+    bgUrl.value = `/images/cyber.jpg`;
   } else if (type == 3) {
     // bgUrl.value = "https://api.vvhan.com/api/wallpaper/acg";
-    bgUrl.value = `/images/background1.jpg`
+    bgUrl.value = `/images/background1.jpg`;
   }
 };
 
